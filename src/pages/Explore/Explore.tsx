54abbf0bbee4f1a326a0568a4e608534e5b802f2
Willma <65773478+willmas-here@users.noreply.github.com>
2023-01-21 14:41:44 -0500

    Implement Explore Page Design

--- src/pages/Explore/Explore.tsx
+++ src/pages/Explore/Explore.tsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import ExploreResult from "./ExploreResult";
 
+import styles from "./Explore.module.css";
+import { FaSearch } from "react-icons/fa";
+
 const Explore = () => {
   const [inputValue, setInputValue] = useState("");
   const [loading, setLoading] = useState(false);
@@ -12,13 +15,15 @@ const Explore = () => {
     setData("data");
   };
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <h1>Explore skills for</h1>
+    <div className={styles.layout}>
+      <form onSubmit={handleSubmit} className={styles.searchContainer}>
+        <h1 className={styles.heading}>Explore skills for</h1>
         <input
+          className={styles.input}
           value={inputValue}
           onChange={(e) => setInputValue(e.target.value)}
         />
+        <FaSearch className={styles.searchIcon} />
       </form>
       {loading ? <p>loading</p> : data ? <ExploreResult /> : null}
     </div>
--- src/pages/Explore/ExploreResult.tsx
+++ src/pages/Explore/ExploreResult.tsx
@@ -1,7 +1,35 @@
 import React from "react";
+import SkillCheckbox from "../../components/SkillCheckbox/SkillCheckbox";
+
+import styles from "./ExploreResult.module.css";
 
 const ExploreResult = () => {
-  return <section>ExploreResult</section>;
+  return (
+    <section className={styles.layout}>
+      <div className={styles.skillSection}>
+        <h2 className={styles.heading}>You've got these skills!</h2>
+        <div className={styles.skillsContainer}>
+          <SkillCheckbox />
+          <SkillCheckbox />
+          <SkillCheckbox />
+          <SkillCheckbox />
+          <SkillCheckbox />
+          <SkillCheckbox />
+        </div>
+      </div>
+      <div className={styles.skillSection}>
+        <h2 className={styles.heading}>You could work on these skills!</h2>
+        <div className={styles.skillsContainer}>
+          <SkillCheckbox />
+          <SkillCheckbox />
+          <SkillCheckbox />
+          <SkillCheckbox />
+          <SkillCheckbox />
+          <SkillCheckbox />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default ExploreResult;
