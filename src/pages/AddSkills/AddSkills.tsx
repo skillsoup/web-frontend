4776a9efd98288e824806e7685db3acb35d570aa
Willma <65773478+willmas-here@users.noreply.github.com>
2023-01-22 02:07:59 -0500

    Multiquestion Questionaire

--- src/pages/AddSkills/AddSkills.tsx
+++ src/pages/AddSkills/AddSkills.tsx
@@ -7,37 +7,76 @@ import Rocket from "../../assets/doohickies/graphic_rocket.png";
 import Worm from "../../assets/doohickies/graphic_worm.png";
 import Circles from "../../assets/doohickies/graphic_circles.png";
 import styles from "./AddSkills.module.css";
+import { SkillCategory } from "../../types/Skill";
+import { useLocalStorage } from "../../utils/useLocalStorage";
+
+const questions: Record<SkillCategory, string> = {
+  communication:
+    "How would you handle a situation where you noticed your supervisor made an error in a report or presentation?",
+  resilience:
+    "How do you stay motivated and engaged during long-term projects or tasks?",
+  teamwork:
+    "Can you give an example of a successful team project you have been a part of and your role in it?",
+  organization: "How do you prioritize daily work tasks?",
+  technical: "Tell me about your technical background.",
+} as const;
 
 interface InputPageProps {
-  textInput: string;
-  setTextInput: React.Dispatch<React.SetStateAction<string>>;
+  formState: Record<SkillCategory, string>;
+  onChange: React.ChangeEventHandler<HTMLTextAreaElement>;
   onClick: (e: React.MouseEvent) => void;
 }
-const InputPage = ({ setTextInput, textInput, onClick }: InputPageProps) => (
-  <>
+const InputPage = ({ onChange, formState, onClick }: InputPageProps) => (
+  <div className={styles.questionsLayout}>
     <div className={styles.headingContainer}>
       <h1 className={styles.heading}>Tell us about yourself</h1>
       <p className={styles.subheading}>
         Our AI assistant will personalize your skillset list in 10 seconds
       </p>
     </div>
-    <textarea
-      className={styles.textInput}
-      value={textInput}
-      onChange={(e) => setTextInput(e.target.value)}
-      placeholder="What skills do you have?"
-    />
+    <div className={styles.questionsContainer}>
+      {Object.getOwnPropertyNames(questions).map((category, index) => (
+        <label className={styles.label} key={index}>
+          {questions[category as SkillCategory]}
+          <textarea
+            className={styles.textInput}
+            value={formState[category as SkillCategory]}
+            onChange={onChange}
+            name={category}
+          />
+        </label>
+      ))}
+    </div>
+
     <button className={styles.button} onClick={onClick}>
       Submit
     </button>
-  </>
+  </div>
 );
 
 const LoadingPage = () => {
   return (
-    <h1 className={styles.heading}>
-      Please give us a second to get your result...
-    </h1>
+    <div className={styles.layout}>
+      <h1 className={styles.heading}>
+        Please give us a second to get your result...
+      </h1>
+    </div>
+  );
+};
+const ErrorPage = () => {
+  const navigate = useNavigate();
+  return (
+    <div className={styles.layout}>
+      <h1 className={styles.heading}>An error occured :(</h1>
+      <button
+        className={styles.button}
+        onClick={() => {
+          navigate("..");
+        }}
+      >
+        Retry
+      </button>
+    </div>
   );
 };
 
@@ -47,7 +86,7 @@ const SuccessPage = () => {
     navigate("/profile");
   };
   return (
-    <>
+    <div className={styles.layout}>
       <h1 className={styles.heading}>
         New skillsets were put in your profile!
       </h1>
@@ -56,54 +95,66 @@ const SuccessPage = () => {
         View Profile
       </button>
       <img className={styles.imgDog} src={Dog} />
-    </>
+    </div>
   );
 };
 
 const AddSkills = () => {
   const navigate = useNavigate();
-  const [textInput, setTextInput] = useState("");
+  const [skillData, setSkillData] = useLocalStorage("skillData");
+  const [formState, setFormState] = useState<Record<SkillCategory, string>>({
+    communication: "",
+    organization: "",
+    resilience: "",
+    teamwork: "",
+    technical: "",
+  });
 
+  const onFormChange: React.ChangeEventHandler = async (
+    e: React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
+    setFormState((current) => {
+      return { ...current, [e.target.name]: e.target.value };
+    });
+  };
   const onClick = async (e: React.MouseEvent) => {
     navigate("loading");
 
     let res: Response;
     try {
-      res = await fetch("https://skillsoup.onrender.com/", {
+      res = await fetch("http://localhost:3000/user/calculate_score", {
         method: "POST",
-        body: textInput,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formState),
       });
       if (!res.ok) {
         return navigate("error", { replace: true });
       }
+      const data = await res.json();
+      setSkillData(JSON.stringify(data));
     } catch (error) {
       console.error(error);
       return navigate("error", { replace: true });
     }
-
-    const data = await res.json();
-    localStorage.setItem("skillData", JSON.stringify(data));
     navigate("success", { replace: true });
   };
   return (
-    <div className={styles.layout}>
-      <Routes>
-        <Route
-          index
-          element={
-            <InputPage
-              onClick={onClick}
-              setTextInput={setTextInput}
-              textInput={textInput}
-            />
-          }
-        />
-        <Route path="loading" element={<LoadingPage />} />
-        <Route path="success" element={<SuccessPage />} />
-        <Route path="error" element={<div>an error occured</div>} />
-        <Route path="*" element={<NoMatch />} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route
+        index
+        element={
+          <InputPage
+            onClick={onClick}
+            onChange={onFormChange}
+            formState={formState}
+          />
+        }
+      />
+      <Route path="loading" element={<LoadingPage />} />
+      <Route path="success" element={<SuccessPage />} />
+      <Route path="error" element={<ErrorPage />} />
+      <Route path="*" element={<NoMatch />} />
+    </Routes>
   );
 };
 
--- src/types/Skill.ts
+++ src/types/Skill.ts
@@ -1,16 +1,6 @@
-export type SkillType = "organization" | "personality";
-
-export type OrganizationSkills =
-  | "Goal setting"
-  | "Decision making"
-  | "Strategy thinking"
-  | "Collaboration"
-  | "Time management"
-  | "Effective communication";
-
-export type PersonalitySkills = "Amiable";
-
-export type TSkills = {
-  organization: OrganizationSkills[];
-  personality: PersonalitySkills[];
-};
+export type SkillCategory =
+  | "communication"
+  | "resilience"
+  | "teamwork"
+  | "organization"
+  | "technical";
